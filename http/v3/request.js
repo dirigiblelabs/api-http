6a09ff6281490dd5ef7a63bef15f1f1f766efdfe
delchev <delchevn@gmail.com>
2017-06-07 08:22:12 -0400

    test println

--- http/v3/request.js
+++ http/v3/request.js
@@ -1,1 +1,2 @@
-org.eclipse.dirigible.api.v3.http.HttpRequestFacade.getMethod();
+var method = org.eclipse.dirigible.api.v3.http.HttpRequestFacade.getMethod();
+java.lang.System.out.println("------------->" + method);
