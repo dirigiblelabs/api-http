a2a8847c3593c2cc172eb6555605a5f5202e81a3
ThuF <jordan.p.pavlov@gmail.com>
2020-04-05 17:39:58 +0300

    Adds "api-modules" Extension

---
+++ http/extensions/modules.js
@@ -0,0 +1,27 @@
+exports.getContent = function() {
+	return [{
+		name: "http/v4/client",
+		description: "HTTP Client API"
+	}, {
+		name: "http/v4/clientAsync",
+		description: "HTTP Client Async API"
+	}, {
+		name: "http/v4/request",
+		description: "HTTP Request API"
+	}, {
+		name: "http/v4/response",
+		description: "HTTP Response API"
+	}, {
+		name: "http/v4/rs-data",
+		description: "HTTP RS-Data API"
+	}, {
+		name: "http/v4/rs",
+		description: "HTTP RS API"
+	}, {
+		name: "http/v4/session",
+		description: "HTTP Session API"
+	}, {
+		name: "http/v4/upload",
+		description: "HTTP Upload API"
+	}];
+};
