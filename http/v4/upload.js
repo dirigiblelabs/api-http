224ca63750cdfa6b442eefd0b3e7b475b936ade4
Ayhan Rashidov <36267628+aihanrashidov@users.noreply.github.com>
2021-08-04 13:15:12 +0300

    Update upload.js

--- http/v4/upload.js
+++ http/v4/upload.js
@@ -15,104 +15,104 @@
  * Note: This module is supported only with the Mozilla Rhino engine
  */
 
- var streams = require("io/v4/streams");
- var bytes = require("io/v4/bytes");
- 
- exports.isMultipartContent = function() {
-	 var result = org.eclipse.dirigible.api.v3.http.HttpUploadFacade.isMultipartContent();
-	 return result;
- };
- 
- exports.parseRequest = function() {
-	 var fileItems = new FileItems();
-	 var native = org.eclipse.dirigible.api.v3.http.HttpUploadFacade.parseRequest();
-	 fileItems.native = native;
-	 return fileItems;
- };
- 
- /**
-  * FileItems object
-  */
- function FileItems() {
- 
-	 this.get = function(index) {
-		 var fileItem = new FileItem();
-		 var native = this.native.get(index);
-		 fileItem.native = native;
-		 return fileItem;
-	 };
- 
-	 this.size = function() {
-		 return this.native.size();
-	 };
- };
- 
- /**
-  * FileItem object
-  */
- function FileItem() {
- 
-	 this.getInputStream = function() {
-		 var inputStream = new streams.InputStream();
-		 var native = this.native.getInputStream();
-		 inputStream.native = native;
-		 return inputStream;
-	 };
- 
-	 this.getContentType = function() {
-		 return this.native.getContentType();
-	 };
- 
-	 this.getName = function() {
-		 return this.native.getName();
-	 };
- 
-	 this.getSize = function() {
-		 return this.native.getSize();
-	 };
- 
-	 this.getBytes = function() {
-		 var data = this.native.get();
-		 return bytes.toJavaScriptBytes(data);
-	 };
- 
-	 this.getBytesNative = function() {
-		 var data = this.native.get();
-		 return data;
-	 };
- 
-	 this.getText = function() {
-		 return this.native.getString();
-	 };
- 
-	 this.isFormField = function() {
-		 return this.native.isFormField();
-	 };
- 
-	 this.getFieldName = function() {
-		 return this.native.getFieldName();
-	 };
- 
-	 this.getHeaders = function() {
-		 var headers = new Headers();
-		 var native = this.native.getHeaders();
-		 headers.native = native;
-		 return headers;
-	 };
- 
- }
- 
- /**
-  * Headers object
-  */
- function Headers() {
- 
-	 this.getHeaderNames = function() {
-		 return org.eclipse.dirigible.api.v3.http.HttpUploadFacade.headerNamesToList(this.native.getHeaderNames());
-	 };
- 
-	 this.getHeader = function(headerName) {
-		 return this.native.getHeader(headerName);
-	 }
- };
- 
+var streams = require("io/v4/streams");
+var bytes = require("io/v4/bytes");
+
+exports.isMultipartContent = function() {
+	var result = org.eclipse.dirigible.api.v3.http.HttpUploadFacade.isMultipartContent();
+	return result;
+};
+
+exports.parseRequest = function() {
+	var fileItems = new FileItems();
+	var native = org.eclipse.dirigible.api.v3.http.HttpUploadFacade.parseRequest();
+	fileItems.native = native;
+	return fileItems;
+};
+
+/**
+ * FileItems object
+ */
+function FileItems() {
+
+	this.get = function(index) {
+		var fileItem = new FileItem();
+		var native = this.native.get(index);
+		fileItem.native = native;
+		return fileItem;
+	};
+
+	this.size = function() {
+		return this.native.size();
+	};
+};
+
+/**
+ * FileItem object
+ */
+function FileItem() {
+
+	this.getInputStream = function() {
+		var inputStream = new streams.InputStream();
+		var native = this.native.getInputStream();
+		inputStream.native = native;
+		return inputStream;
+	};
+
+	this.getContentType = function() {
+		return this.native.getContentType();
+	};
+
+	this.getName = function() {
+		return this.native.getName();
+	};
+
+	this.getSize = function() {
+		return this.native.getSize();
+	};
+
+	this.getBytes = function() {
+		var data = this.native.get();
+		return bytes.toJavaScriptBytes(data);
+	};
+
+	this.getBytesNative = function() {
+		var data = this.native.get();
+		return data;
+	};
+
+	this.getText = function() {
+		return this.native.getString();
+	};
+
+	this.isFormField = function() {
+		return this.native.isFormField();
+	};
+
+	this.getFieldName = function() {
+		return this.native.getFieldName();
+	};
+
+	this.getHeaders = function() {
+		var headers = new Headers();
+		var native = this.native.getHeaders();
+		headers.native = native;
+		return headers;
+	};
+
+}
+
+/**
+ * Headers object
+ */
+function Headers() {
+
+	this.getHeaderNames = function() {
+		return org.eclipse.dirigible.api.v3.http.HttpUploadFacade.headerNamesToList(this.native.getHeaderNames());
+	};
+
+	this.getHeader = function(headerName) {
+		return this.native.getHeader(headerName);
+	}
+};
+
